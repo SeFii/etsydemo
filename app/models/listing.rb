8b40fb985790a648eba0a5ef5cbe88dc5d002539
SeFii <sefizoso@gmail.com>
2014-06-30 15:27:08 +0200

    Little Fail

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,12 +1,12 @@
 class Listing < ActiveRecord::Base
-	if Rails.env.developement?
+	if Rails.env.development?
 		has_attached_file :image, :styles => { :medium => "300x>", :thumb => "100x100>" }, :default_url => "default.png"
+		validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	else
-	  has_attached_file 
-	  	:image, :styles => { :medium => "300x>", :thumb => "100x100>" }, :default_url => "default.png", 
-	  	:storage => :dropbox,
-	    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-	    :path => ":style/:id_:filename"
+	  has_attached_file :image, :styles => { :medium => "300x>", :thumb => "100x100>" }, :default_url => "default.png", 
+	  :storage => :dropbox, 
+	  :dropbox_credentials => Rails.root.join("config/dropbox.yml"), 
+	  :path => ":style/:id_:filename"
 	  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	end  
 end
